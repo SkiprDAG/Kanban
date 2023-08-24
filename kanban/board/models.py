19380358ada7a913b0f2f51570da2619c2f63cf4
prokopchuk_va <202099423@irgups.ru>
2023-08-24 08:55:12 +0800

    add filterset_fields and auto added

--- kanban/board/models.py
+++ kanban/board/models.py
@@ -6,7 +6,6 @@ from django.db import models
 class Board(models.Model):
     users = models.ManyToManyField('user.CustomUser')
     owner = models.ForeignKey('user.CustomUser', on_delete=models.DO_NOTHING, related_name='owner')
-    # codename = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     column = models.ManyToManyField('board.Column', blank=True)
 
@@ -15,7 +14,6 @@ class Board(models.Model):
 
 
 class Column(models.Model):
-    # codename = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     number = models.IntegerField()
     name = models.CharField(max_length=255)
     card = models.ManyToManyField('board.Task', blank=True)
@@ -25,7 +23,6 @@ class Column(models.Model):
 
 
 class Task(models.Model):
-    # codename = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     responsible = models.ForeignKey('user.CustomUser', on_delete=models.DO_NOTHING, blank=True, null=True)
     name = models.CharField(max_length=255)
     title = models.TextField()
--- kanban/board/views.py
+++ kanban/board/views.py
@@ -15,6 +15,7 @@ class BoardViewSet(mixins.CreateModelMixin,
                         GenericViewSet):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+    filterset_fields = ['owner', ]
 
 
 class ColumnViewSet(mixins.CreateModelMixin,
@@ -26,6 +27,12 @@ class ColumnViewSet(mixins.CreateModelMixin,
     queryset = Column.objects.all()
     serializer_class = ColumnSerializer
 
+    def perform_create(self, serializer):
+        s = serializer.save()
+        board_id = self.request.data['board']
+        board = Board.objects.get(id=board_id)
+        board.column.add(s)
+
 
 class CardViewSet(mixins.CreateModelMixin,
                         mixins.UpdateModelMixin,
@@ -35,3 +42,9 @@ class CardViewSet(mixins.CreateModelMixin,
                         GenericViewSet):
     queryset = Task.objects.all()
     serializer_class = CardSerializer
+
+    def perform_create(self, serializer):
+        s = serializer.save()
+        column_id = self.request.data['column']
+        column = Column.objects.get(id=column_id)
+        column.card.add(s)
